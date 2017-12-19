f9a01978412864c0dfa2ac28f8a3eb3bddfa6577
Felipe Dias <faadias@gmail.com>
2017-12-19 07:25:18 -0200

    Início da tela de login

---
+++ webapp/js/login.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+	window.setTimeout(function() {
+		//FIXME trocar este dummy por uma chamada para o login (ver se está logado)
+		$(".content").removeClass("loading");
+	}, 3000);
+});
