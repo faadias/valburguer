02c2f7b66ef1f098450f1cbfedc7b2be3a703c54
Felipe Dias <faadias@gmail.com>
2017-12-20 21:21:36 -0200

    Tela de pedidos
    - Resolvido problema do botão do cart em navegadores webkit mobile (a barra fazia com que ele ficasse fora da viewport);
    - Resolvido problemas de layout envolvendo o modal em navegadores mobile;
    - Salva dos itens do carrinho no localStorage e recuperação do mesmo.

--- webapp/js/orders.js
+++ webapp/js/orders.js
@@ -3,10 +3,30 @@ const currencyFormatter = Intl.NumberFormat("pt-br", { style : "currency", curre
 
 const MIN_QUANTITY = 1;
 const MAX_QUANTITY = 99;
+const IDLE_TIME_BEFORE_CLEAR_CART_IN_MS = 1000*60*15; //15 minutos
 
+let cartTotal = 0;
 let products = [];
 
 $(document).ready(function() {
+	let lastActivity = localStorage.getItem("lastActivity");
+	if (lastActivity == null || new Date().getTime() - parseInt(lastActivity) > IDLE_TIME_BEFORE_CLEAR_CART_IN_MS) {
+		localStorage.clear();
+	}
+	else {
+		localStorage.removeItem("lastActivity");
+		let total = Object.keys(localStorage)
+			.map(id => {
+				let json = JSON.parse(localStorage.getItem(id));
+				return json.price * json.quantity;
+			})
+			.reduce((acum,value) => acum+value, 0);
+		
+		updateCart(total);
+	}
+	
+	localStorage.setItem("lastActivity", new Date().getTime());
+	
 	$("#overlay").show();
 	
 	bindActions();
@@ -26,15 +46,22 @@ function bindActions() {
 	
 	$(".action-back").on("click", closeModal);
 	
-	$(".action-minus").on("click", changeQuantity);
-	$(".action-plus").on("click", changeQuantity);
+	$("#product-detail-modal").on("click", ".action-minus", changeQuantity);
+	$("#product-detail-modal").on("click", ".action-plus", changeQuantity);
 	
-	$("#product-detail-modal .modal-action").click(addToCart);
+	$("#product-detail-modal").on("click", ".modal-action", addToCart);
+	$("#product-detail-modal").on("change", ".quantity", updateTotalPrice);
 }
 
 function openSettings() {
 	//TODO
 	alert("Settings não implementado!");
+}
+
+function updateTotalPrice(e) {
+	let quantity = parseInt(e.target.value);
+	let price = currencyToFloat($(e.delegateTarget).find(".product-price").html());
+	$(e.delegateTarget).find(".product-total-price").html(currencyFormatter.format(quantity*price));
 }
 
 function openSection() {
@@ -46,12 +73,16 @@ function openSection() {
 function openProductDetails(e) {
 	let data = $(e.currentTarget).data();
 	
+	let storedData = JSON.parse(localStorage.getItem(data.id)) || {quantity : MIN_QUANTITY, obs : ""};
+	$("#product-detail-modal .product-obs").val(storedData.obs);
+	$("#product-detail-modal .quantity").val(storedData.quantity);
+	
 	$("#product-detail-modal .product-id").val(data.id);
 	$("#product-detail-modal .product-pic").attr("src", data.pic || "../imgs/nopic.png");
 	$("#product-detail-modal .product-name").html(data.name);
 	$("#product-detail-modal .product-description").html(data.description || "");
-	$("#product-detail-modal .product-price").html(data.price);
-	$("#product-detail-modal .order-obs").val("");
+	$("#product-detail-modal .product-price").html(currencyFormatter.format(data.price));
+	$("#product-detail-modal .product-total-price").html(currencyFormatter.format(data.price*storedData.quantity));
 	
 	$("#product-detail-modal").show();
 }
@@ -60,12 +91,25 @@ function closeModal() {
 	$("article.modal").hide();
 }
 
+function currencyToFloat(currency) {
+	return parseFloat(currency.replace(",",".").replace(/[^0-9.]/g,""));
+}
+
 function changeQuantity(e) {
+	let $context = $(e.delegateTarget);
 	let increment = $(e.target).hasClass("action-minus") ? -1 : 1;
-	let quantity = parseInt($(e.target.parentNode).children(".quantity").html());
+	let quantity = parseInt($(e.target.parentNode).children(".quantity").val()) + increment;
+	let id = parseInt($context.find(".product-id").val());
 	
-	quantity = Math.min(Math.max(MIN_QUANTITY, quantity+increment), MAX_QUANTITY);
-	$(e.target.parentNode).children(".quantity").html(quantity);
+	let storedData = JSON.parse(localStorage.getItem(id));
+	if (quantity <= 0 && storedData && confirm("Deseja remover este produto do carrinho?")) {
+		updateCart(-storedData.quantity*storedData.price);
+		closeModal();
+		return;
+	}
+	
+	quantity = Math.max(Math.min(quantity, MAX_QUANTITY), MIN_QUANTITY);
+	$(e.target.parentNode).children(".quantity").val(quantity).change();
 }
 
 function handleCategories(response) {
@@ -118,10 +162,37 @@ function initScreen() {
 
 function addToCart() {
 	let id = parseInt($("#product-detail-modal .product-id").val());
-	let quantity = parseInt($("#product-detail-modal .quantity").html());
-	let obs = $("#product-detail-modal .order-obs").val();
-
+	let price = currencyToFloat($("#product-detail-modal .product-price").html());
+	let quantity = parseInt($("#product-detail-modal .quantity").val());
+	let obs = $("#product-detail-modal .product-obs").val();
+	
+	let data = JSON.parse(localStorage.getItem(id));
+	if (data !== null) {
+		updateCart(-data.quantity*data.price);
+	}
+	
+	localStorage.setItem(id, JSON.stringify({quantity, obs, price}));
+	
+	updateCart(quantity*price);
+	
 	closeModal();
+}
+
+function emptyCart() {
+	localStorage.clear();
+	updateCart(-cartTotal);
+}
+
+function updateCart(increment) {
+	cartTotal += increment;
+	$("#cart-total-price").html(currencyFormatter.format(cartTotal));
+	
+	if (cartTotal === 0) {
+		$("#action-cart").addClass("disabled");
+	}
+	else {
+		$("#action-cart").removeClass("disabled");
+	}
 }
 
 function isRestError(response) {
